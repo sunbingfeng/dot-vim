2d43cdd7cc97d853700edb511a823dad17a94cca
bingfeng.sun <bingfeng.sun@dorabot.com>
2021-05-12 19:02:37 +0800

    Add install script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/env zsh
+
+echo "Please go away for a cup for coffee, it may take for a while."
+if which apt-get >/dev/null; then
+	sudo apt-get update
+	sudo add-apt-repository ppa:jonathonf/vim
+	sudo apt-get install -y vim exuberant-ctags clang-format
+	pip3 install pathtools
+else
+	echo "Platforms other than Ubuntu are not supported currently."
+	exit
+fi
+
+# Save old vim configurations
+mv -f ~/.vim ~/.vim_old
+mv -f ~/.vimrc ~/.vimrc_old
+
+# Clone current repo and install plugins
+git clone https://github.com/sunbingfeng/dot-vim.git ~/.vim && cd ~/.vim
+ln -s ~/.vim/.vimrc ~/.vimrc
+
+# Install vim-plug package manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+temp_file="$(mktemp -t "install.XXXXXXXXXX")"
+echo "Installing the plugins" > $temp_file
+echo "Please wait for a while." >> $temp_file
+
+vim $temp_file -c "PlugInstall" -c "q" -c "q"
+rm -f -- "$temp_file"
+
+echo "Install Successed."
